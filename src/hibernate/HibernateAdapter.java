769bc90d3357098ecd0924d0c69ce0fc10bb8256
unknown <cafebo85@gmail.com>
2019-02-28 18:58:42 +0100

    Inicio

---
+++ src/hibernate/HibernateAdapter.java
@@ -0,0 +1,112 @@
+package hibernate;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import connections.SessionFactoryUtil;
+/**Clase genèrica amb la que podem implementar el crud amb hibernate.
+* Fent una instancia d'un objecte d'aquesta classe en cadascuna de les classes on volguem implementar
+* el crud. El constructor rep un Object String que utilitzarem per fer la query que ens retorna tots els
+* registres d'una taula. En els mètodes en els que necessitem un id, utiliztzem un objexte Serializable, que és el que
+* demana el mètode del hibernate
+*/ 
+public class HibernateAdapter<T> {
+
+	private Session session;
+	private Transaction tx;
+	private String hql;
+
+	public HibernateAdapter(String hql){
+		this.hql = hql;
+	}
+	private void openSession() {
+		session = SessionFactoryUtil.getSessionFactory().openSession();
+		tx = null;
+		try {
+			tx = session.beginTransaction();
+		}catch(HibernateException e) {
+			if(null != tx) tx.rollback();
+		}
+	}
+
+	public T readObject(Class<T> objectClass, Serializable id) {
+		openSession();
+		T object = null;
+		try {
+			object = session.get(objectClass, id);
+			tx.commit();
+		}catch(HibernateException e) {
+			if(null != tx) tx.rollback();
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		return object;
+	}
+
+	public Integer addObject(T object) {
+		openSession();
+		try {
+			session.save(object);
+			tx.commit();
+		}catch(HibernateException e) {
+			if(null != tx) tx.rollback();
+			return 0;
+		}finally {
+			session.close();
+		}
+		return 1;
+	}
+
+	public Integer updateObject(T object) {
+		openSession();
+		try {
+			session.update(object);
+			tx.commit();
+		}catch(HibernateException e) {
+			if(null != tx) tx.rollback();
+			return 0;
+		}finally {
+			session.close();
+		}
+		return 1;
+	}
+
+	public T deleteObject(Class<T> objectClass, Serializable id ) {
+		openSession();
+		T object = null;
+		try {
+			object = session.get(objectClass, id);
+			if(object != null)
+			session.delete(object);
+			tx.commit();
+		}catch(HibernateException e) {
+			if(null != tx) tx.rollback();
+		}finally {
+			session.close();
+		}
+		return object;
+	}
+	
+	public ArrayList<T> readAll() {
+		openSession();
+		ArrayList<T> objects = new ArrayList<T>();
+		try {
+			objects  = (ArrayList<T>) session.createQuery(hql).list();
+			tx.commit();
+		}catch(HibernateException e) {
+			if(null != tx) tx.rollback();
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		return objects;
+	}
+
+	
+
+}
---
+++ src/hibernate/WatchedListHibernate.java
@@ -0,0 +1,37 @@
+package hibernate;
+
+import java.util.ArrayList;
+
+import iFaces.WatchedListDAO;
+import pojos.Watchedlist;
+
+public class WatchedListHibernate implements WatchedListDAO {
+
+	private HibernateAdapter<Watchedlist> adapter = new HibernateAdapter<Watchedlist>("FROM Watchedlist");
+	@Override
+	public Watchedlist getItem(int itemId) {
+		return adapter.readObject(Watchedlist.class, itemId);
+	}
+
+	@Override
+	public Integer addItem(Watchedlist watchedlist) {
+		return adapter.addObject(watchedlist);
+	}
+
+	@Override
+	public Watchedlist deleteItem(Integer itemId) {
+		return adapter.deleteObject(Watchedlist.class, itemId);
+	}
+
+	@Override
+	public int updateItem(Watchedlist watchedlist) {
+		return adapter.updateObject(watchedlist);
+	}
+
+	@Override
+	public ArrayList<Watchedlist> getWatchedlist() {
+		return adapter.readAll();
+	}
+
+	
+}
